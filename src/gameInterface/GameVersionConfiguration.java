d24473aae11ab713b276a5e283045da549160d17
Steven Pham <steven.pham.8.2000@gmail.com>
2021-12-06 23:18:10 -0500

    Fix no JSON entry for railroad

--- src/gameInterface/GameVersionConfiguration.java
+++ src/gameInterface/GameVersionConfiguration.java
@@ -3,7 +3,6 @@ package gameInterface;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
